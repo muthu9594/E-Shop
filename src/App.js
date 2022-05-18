64a0942a10c5eeec5dbfd1c66b78c5344aae178b
muthu9594 <mukeshselvam9@gmail.com>
2022-05-18 15:03:07 +0530

    'add_Header_in_Seperate_component'

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 
 import './App.css';
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+
+import Header from './Components/Header/Header.js'
 
 function App() {
   return (
     <div className="App">
-      <div className="header__logo">
-
-      </div>
-      <div className="header__search">
-
-      </div>
-      <div className="header__nav">
-        <div className="nav__item">
-          <span className="nav__itemLineOne">Hello Guest</span>
-          <span className="nav__itemLineTwo">Sign In</span>
-        </div>
-        <div className="nav__item">
-          <span className="navitemLineOne">Your</span>
-          <span className="navitemLineTwo">Shop</span>
-        </div>
-        <div className="nav__item">
-          <ShoppingBasketIcon/>
-          <span className="nav__itemLineTwo">0</span>
-          
-        </div>
-      </div>
-
+      <Header/>
     </div>
   );
 }
